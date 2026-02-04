2830f0429fda7bf0865fd33f9a091decc62d2ed9
Huyvudeptrai <dunghuy05@gmail.com>
2026-02-04 15:05:32 +0700

    Add alembic migration

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,12 @@
 from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     app_name: str = "Locket Clone"
-    database_url: str = "sqlite:///./app.db"
+    database_url: str = ""
     secret_key:str = "CHANGE_ME"
     algorithm: str = "HS256"
+    model_config = SettingsConfigDict(env_file=".env")
 
-    class Config:
-        env_file = ".env"
 
 settings = Settings()
